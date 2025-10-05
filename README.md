65e7d678058e1fa36ad9d7c5111b7098ac7eabf0
adrian.addesso <adrian.addesso@gmail.com>
2025-10-05 09:34:13 -0300

    Implementacion de Table

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # TP3: Vue CLI - Router / Formularios
-
+<!--Consigna https://drive.google.com/drive/folders/148QrOGsNk2EPVBQErMNhCR_t1Qba-c-M -->
 Realizar un desarrollo web front-end empleando Vue CLI que presente las
 siguientes vistas manejadas por un router, seleccionadas desde una barra de
 navegación:
@@ -10,11 +10,12 @@ caracteres, edad entre 18 y 120 años e email válido. Los datos válidos ingres
 por el formulario, se deben representar en un tabla por debajo del mismo, en el
 mismo momento del ingreso.
 
-[] Realizar un componente que pida los objetos almacenados en un recurso de
+[x] Realizar un componente que pida los objetos almacenados en un recurso de
 mockapi.io llamado usuarios, que contengan datos aleatorios con las claves:
 nombre, email y número de teléfono. Se representarán en una tabla en la vista.
 Para la comunicación con mockapi.io utilizar axios.
 Utilizar Bootstrap como framework css del sitio.
+<!--Magic link for API cloning https://mockapi.io/clone/68db27bd23ebc87faa321d8d-->
 
 [] Enviar por email a danielsanchez68@hotmail.com el link al repo de cada uno de
 ustedes con el ejercicio resuelto
--- src/components/Table.vue
+++ src/components/Table.vue
@@ -1,20 +1,83 @@
 <template>
-<p>table</p>
+    <section class="containerd mt-2">
+        <div v-if="noFetch" class="alert alert-light col-8" role="alert"> {{ wText }}</div>
+        <div v-else-if="errorCarga" class="alert alert-danger col-8" role="alert"> {{ aText }}</div>
+
+        <table v-else class="table table-striped table-hover table-responsive">
+            <caption>Última actualización: {{ tStamp }}</caption>
+            <thead class="table-dark">
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Nombre</th>
+                    <th scope="col">E-Mail</th>
+                    <th scope="col">Teléfono</th>
+                </tr>
+            </thead>
+            <tbody>
+                    <tr v-for="(usuario, index) in usuarios" :key="index">
+                        <td>{{ usuario.id }}</td>
+                        <td>{{ usuario.nombre }}</td>
+                        <td>{{ usuario.email }}</td>
+                        <td>{{ usuario.tel }}</td>
+                    </tr>
+            </tbody>
+        </table>
+        <div class="text-start mt-2">
+            <button class="btn btn-success" @click="obtener">Actualizar</button>
+        </div>
+    </section>
 </template>
 
 <script>
+    import ServicioUsuarios from '../servicios/usuarios';
     export default {
         name: 'Table',
         data() {
             return {
-
+                noFetch: true,
+                errorCarga: false,
+                tStamp: '',
+                wText: "ℹ️ Haga click en actualizar para traer la lista de usuarios.",
+                aText: "🚨 Algo falló al buscar los usuarios. Contacte a soporte.",
+                servUsuarios: new ServicioUsuarios,
+                usuarios: [],
             }
         },
         methods: {
+            async obtener(){
+                try{
+                    const usuarios = await this.servUsuarios.getAll();
+                    console.log(usuarios);
+                    this.usuarios = usuarios;
+                    this.noFetch = false;
+                    this.tStamp = this.getTimestamp();
+                }
+                catch{
+                    this.noFetch = false;
+                    this.errorCarga = true;
+                }
+            },
+            getTimestamp(){
+                const fecha = new Date()
+                const datos = {
+                    weekday: 'long',
+                    year: 'numeric',
+                    month: 'long',
+                    day: 'numeric'
+                }
+                const fechaLocal = fecha.toLocaleDateString('es-ES', datos)
+                const horaLocal = fecha.toLocaleTimeString('es-ES', {
+                    hour: '2-digit',
+                    minute: '2-digit',
+                    second: '2-digit'
+                })
+                return `${fechaLocal} - ${horaLocal}`
+            },
         },
         computed: {
         }
-    }
+    };
+
 </script>
 
 <style scoped>
